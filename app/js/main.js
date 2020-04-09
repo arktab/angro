abb5536cb88a2e4e1d132380618494f7f18e8e79
Alexandr <tabarkevich@gmail.com>
2020-04-09 09:40:46 +0300

    first commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,53 @@
 'use strict';
+//Animation lib AOS
+AOS.init({
+    duration: 1200,
+    easing: 'ease-in-out-back'
+});
+//___________________________________
+(function($) {
+    $(document).ready(function() {
+        // Code
+        var visible = new Boolean(false);
+        $(".button__menu").click(function() {
 
-(function($){
-	$(document).ready(function() {
-		// Code
+            if (visible == false) {
+                setTimeout(function() {
+                    visible = true;
+                    $(".menu").toggleClass("menu--opacity");
+                    $(".bg").toggleClass("menu--opacity");
+                    setTimeout(function() {
+                        $(".menu").toggleClass("menu--none");
+                        $(".bg").toggleClass("menu--none");
+                        $(".sidebar").toggleClass("blend");
+                    }, 200);
 
-	});
+                }, 100);
+            } else {
+                visible = false;
+                setTimeout(function() {
+                    $(".menu").toggleClass("menu--none");
+                    $(".bg").toggleClass("menu--none");
+                    $(".sidebar").toggleClass("blend");
+                    setTimeout(function() {
+                        $(".menu").toggleClass("menu--opacity");
+                        $(".bg").toggleClass("menu--opacity");
+                    }, 200);
+
+                }, 100);
+            }
+
+        });
+        $(window).scroll(function() {
+            $(".menu").addClass("menu--opacity");
+            $(".bg").addClass("menu--opacity");
+            setTimeout(function() {
+                setTimeout(function() {
+                    $(".menu").addClass("menu--none");
+                    $(".bg").addClass("menu--none");
+                }, 200);
+                $(".sidebar").addClass("blend");
+            }, 200);
+        });
+    });
 })(jQuery);
--- gulpfile.js
+++ gulpfile.js
@@ -14,140 +14,170 @@ const uglify = require('gulp-uglify');
 const gulpIf = require('gulp-if');
 const wait = require('gulp-wait');
 const notify = require('gulp-notify');
+const pug = require('gulp-pug');
 
 
 let paths = {
-	css: {
-		src: "app/scss/**/*.scss",
-		dest: "app/css",
-		destBuild: "dist/css"
-	},
-	html: {
-		src: "app/*.html",
-		dest: "dist"
-	},
-	js: {
-		src: "app/js/**/*"
-	},
-	images: {
-		src: "app/images/**/*",
-		dest: "dist/images"
-	},
-	fonts: {
-		src: "app/css/fonts/**/*",
-		dest: "dist/css/fonts"
-	}
+    pug: {
+        src: "app/pug/**/*.pug",
+        dest: "app/",
+        destBuild: "app/"
+    },
+    css: {
+        src: "app/scss/**/*.scss",
+        dest: "app/css",
+        destBuild: "dist/css"
+    },
+    html: {
+        src: "app/*.html",
+        dest: "dist"
+    },
+    js: {
+        src: "app/js/**/*"
+    },
+    images: {
+        src: "app/images/**/*",
+        dest: "dist/images"
+    },
+    fonts: {
+        src: "app/css/fonts/**/*",
+        dest: "dist/css/fonts"
+    }
 };
 
 let sources = {
-	css: [
-		'./node_modules/bootstrap/dist/css/bootstrap.min.css'
-	],
-	js: [
-		'./node_modules/jquery/dist/jquery.min.js',
-		'./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
-	]
+    css: [
+        './node_modules/bootstrap/dist/css/bootstrap.min.css'
+    ],
+    js: [
+        './node_modules/jquery/dist/jquery.min.js',
+        './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+    ]
 };
 
 // Modules
 function modulesCSS() {
-	return gulp.src(sources.css)
-			   .pipe(gulp.dest('app/css/libs'));
+    return gulp.src(sources.css)
+        .pipe(gulp.dest('app/css/libs'));
 }
 
 function modulesJS() {
-	return gulp.src(sources.js)
-			   .pipe(gulp.dest('app/js/libs'));
+    return gulp.src(sources.js)
+        .pipe(gulp.dest('app/js/libs'));
 }
 
 // BrowserSync
 function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: "./app/"
-    },
-    port: 9000
-  });
-  done();
+    browsersync.init({
+        server: {
+            baseDir: "./app/"
+        },
+        port: 9000
+    });
+    done();
 }
 
 // BrowserSync Reload
 function browserSyncReload(done) {
-  browsersync.reload();
-  done();
+    browsersync.reload();
+    done();
 }
 
 // Error Handling
 function plumbError() {
-  return plumber({
-    errorHandler: function(err) {
-      notify.onError({
-        templateOptions: {
-          date: new Date()
-        },
-        title: "Gulp error in " + err.plugin,
-        message: err.formatted
-      })(err);
-      this.emit('end');
-    }
-  })
+    return plumber({
+        errorHandler: function(err) {
+            notify.onError({
+                templateOptions: {
+                    date: new Date()
+                },
+                title: "Gulp error in " + err.plugin,
+                message: err.formatted
+            })(err);
+            this.emit('end');
+        }
+    })
 }
 
 // CSS task
 function styles() {
-	return gulp.src(paths.css.src)
-	           .pipe(wait(500))
-       		   .pipe(plumbError())
-	           .pipe(sass())
-	           .pipe(rename({ suffix: ".min" }))
-	           .pipe(postcss([autoprefixer(), cssnano()]))
-	           .pipe(gulp.dest(paths.css.dest))
-	           .pipe(browsersync.stream());
+    return gulp.src(paths.css.src)
+        .pipe(wait(500))
+        .pipe(plumbError())
+        .pipe(sass())
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(gulp.dest(paths.css.dest))
+        .pipe(browsersync.stream());
 }
 
 // Optimize Images
 function images() {
-	return gulp.src(`${paths.images.src}.+(png|jpg|jpeg|gif|svg)`)
-			   .pipe(cache(imagemin({interlaced: true})))
-			   .pipe(gulp.dest(paths.images.dest));
+    return gulp.src(`${paths.images.src}.+(png|jpg|jpeg|gif|svg)`)
+        .pipe(cache(imagemin({ interlaced: true })))
+        .pipe(gulp.dest(paths.images.dest));
 }
+//pug
+function render() {
+    return gulp.src(paths.pug.src)
+        .pipe(wait(500))
+        .pipe(plumbError())
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest(paths.pug.dest))
+        .pipe(browsersync.stream());
+}
+// gulp.task('pug', () => {
+//     return gulp.src('app/**/*.pug')
+//         .pipe(pug({
+//             pretty: true
+//         }))
+//         .pipe(gulp.dest('dist/'))
+// })
 
 // Clean dist
 function clean() {
-  return del([paths.html.dest]);
+    return del([paths.html.dest]);
 }
 
 // Move items to dist
 function fonts() {
-  return gulp.src(paths.fonts.src)
-  			 .pipe(gulp.dest(paths.fonts.dest));
+    return gulp.src(paths.fonts.src)
+        .pipe(gulp.dest(paths.fonts.dest));
 };
 
 function distFiles() {
-	return gulp.src(paths.html.src)
-			   .pipe(useref())
-			   .pipe(gulpIf('*.js', uglify()))
-			   .pipe(gulp.src('*.css'))
-			   .pipe(gulp.dest(paths.html.dest));
+    return gulp.src(paths.html.src)
+        .pipe(useref())
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulp.src('*.css'))
+        .pipe(gulp.dest(paths.html.dest));
 }
 
 // Watch files
 function watchFiles() {
-  	gulp.watch(paths.css.src, styles);
-  	gulp.watch(paths.html.src, browserSyncReload);
-  	gulp.watch(paths.js.src, browserSyncReload);
-  	gulp.watch(paths.images.src, images);
+    gulp.watch(paths.css.src, styles);
+    gulp.watch(paths.pug.src, render);
+    gulp.watch(paths.html.src, browserSyncReload);
+    gulp.watch(paths.js.src, browserSyncReload);
+    gulp.watch(paths.images.src, images);
 }
 
 // define complex tasks
 const modules = gulp.parallel(modulesCSS, modulesJS);
-const build = gulp.series(clean, modules, styles, gulp.parallel(distFiles, images, fonts));
+const build = gulp.series(clean, modules, styles, render, gulp.parallel(distFiles, images, fonts));
 const watch = gulp.series(modules, gulp.parallel(watchFiles, browserSync));
 
+//pug
+// gulp.task('default', () => {
+//     gulp.watch('app/**/*.pug', gulp.series('pug'))
+// })
 
 // export tasks
 exports.images = images;
 exports.styles = styles;
+exports.render = render;
 exports.clean = clean;
 exports.fonts = fonts;
 exports.distFiles = distFiles;
