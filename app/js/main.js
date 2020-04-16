40f3d3e0dbd5c7cea01e694c64a50efa8f580ee2
Alexandr <tabarkevich@gmail.com>
2020-04-16 21:25:55 +0300

    boop

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,8 @@
 'use strict';
 //Animation lib AOS
 AOS.init({
-    //duration: 1200,
-    easing: 'ease-in-out-back'
+    //duration: 800,
+    //easing: 'ease-in-out-back'
 });
 //___________________________________
 (function($) {
@@ -15,39 +15,67 @@ AOS.init({
                 setTimeout(function() {
                     visible = true;
                     $(".menu").toggleClass("menu--opacity");
-                    //$(".bg").toggleClass("menu--opacity");
                     setTimeout(function() {
                         $(".menu").toggleClass("menu--is-hide");
-                        //$(".bg").toggleClass("menu--none");
-                        //$(".sidebar").toggleClass("blend");
                     }, 200);
-
                 }, 100);
             } else {
                 visible = false;
                 setTimeout(function() {
                     $(".menu").toggleClass("menu--is-hide");
-                    //$(".bg").toggleClass("menu--none");
-                    //$(".sidebar").toggleClass("blend");
                     setTimeout(function() {
                         $(".menu").toggleClass("menu--opacity");
-                        //$(".bg").toggleClass("menu--opacity");
                     }, 200);
-
                 }, 100);
             }
         });
+        //
+        var width = $(window).width();
+        if (width <= 576) {
+            setTimeout(function() {
+                visible = true;
+                $(".menu").toggleClass("menu--opacity");
+                setTimeout(function() {
+                    $(".menu").toggleClass("menu--is-hide");
+                }, 200);
+            }, 100);
+        }
+
         // $(window).scroll(function() {
-        //     $(".menu").addClass("menu--opacity");
-        //     $(".bg").addClass("menu--opacity");
-        //     setTimeout(function() {
+        //     var d = $('.menu');
+        //     if (d.offset().top < 400) {
         //         setTimeout(function() {
-        //             $(".menu").addClass("menu--none");
-        //             $(".bg").addClass("menu--none");
-        //         }, 200);
-        //         $(".sidebar").addClass("blend");
-        //     }, 200);
+        //             visible = true;
+        //             $(".menu").toggleClass("menu--opacity");
+        //             setTimeout(function() {
+        //                 $(".menu").toggleClass("menu--is-hide");
+        //             }, 200);
+        //         }, 100);
+        //     } else {
+        //         setTimeout(function() {
+        //             $(".menu").toggleClass("menu--is-hide");
+        //             setTimeout(function() {
+        //                 $(".menu").toggleClass("menu--opacity");
+        //             }, 200);
+        //         }, 100);
+        //     }
         // });
+        // $(window).scroll(function() {
+        //     visible = false;
+        //     setTimeout(function() {
+        //         $(".menu").toggleClass("menu--is-hide");
+        //         setTimeout(function() {
+        //             $(".menu").toggleClass("menu--opacity");
+        //         }, 200);
+        //     }, 100);
+        // });
+
+        //MOBILE 
+        //BURGER MENU
+        $('.burger-menu').click(function() {
+            $(this).toggleClass('burger-menu--active');
+            $('body').toggleClass('is-menu-shown');
+        });
 
         //SLIDER__________________________________
         $('.about-slider').slick({
@@ -59,7 +87,15 @@ AOS.init({
             speed: 500,
             cssEase: 'linear',
             draggable: false,
-            slidesToShow: 3
+            slidesToShow: 3,
+            responsive: [{
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            }]
         });
         $('.objects-slider').slick({
             nextArrow: $('.objects-slider__icon'),
@@ -76,9 +112,16 @@ AOS.init({
             autoplaySpeed: 4000,
             infinite: true,
             arrows: true,
-            speed: 500,
+            speed: 1000,
             cssEase: 'linear',
-            slidesToShow: 1
+            fade: true,
+            slidesToShow: 1,
+            responsive: [{
+                breakpoint: 576,
+                settings: {
+                    arrows: false
+                }
+            }]
         });
         $('.apartments-slider').slick({
             nextArrow: $('.apartments-slider__nextbtn'),
@@ -87,8 +130,20 @@ AOS.init({
             infinite: true,
             arrows: true,
             speed: 500,
-            cssEase: 'linear',
-            slidesToShow: 3
+            slidesToShow: 3,
+            responsive: [{
+                    breakpoint: 1180,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                }
+            ]
         });
         //slider button
     });
