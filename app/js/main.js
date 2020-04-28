38320a5c30ac453865526c671db5607064cf0a4e
Alexandr <tabarkevich@gmail.com>
2020-04-28 19:12:05 +0300

    add page gallery; add page gallery-opened;

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,7 @@ AOS.init({
     //duration: 800,
     //easing: 'ease-in-out-back'
 });
-
+lightGallery(document.getElementById('lightgallery'));
 
 //___________________________________
 (function($) {
